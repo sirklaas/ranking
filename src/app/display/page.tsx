b4c81d324fb8643d22b4fe39b1addb36d9a058a1
sirklaas <98842884+sirklaas@users.noreply.github.com>
2026-02-26 11:23:31 +0100

    v2.3: bump all version badges

--- src/app/display/page.tsx
+++ src/app/display/page.tsx
@@ -8,7 +8,7 @@ import '@/modules/fases/auto-register';
 import { FASES, findFaseModule } from '@/modules/fases';
 import { safeJsonStr } from '@/lib/jsonUtils';
 
-const APP_VERSION = 'v2.2a';
+const APP_VERSION = 'v2.3';
 
 interface PlayersByTeam {
   [teamNumber: number]: string[];
